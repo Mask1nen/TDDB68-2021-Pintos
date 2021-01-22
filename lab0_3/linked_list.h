e432e760aea06a3f56732777e9e03786da702c9c
hamra084 <hamra084@tlhw-4-2.ad.liu.se>
2021-01-22 17:00:56 +0100

    lab0.3 klar

---
+++ lab0_3/linked_list.h
@@ -0,0 +1,13 @@
+typedef struct list_item listNode;
+
+void append(listNode *first, int x);
+
+void prepend(listNode *first, int x);
+
+void print(listNode *first);
+
+void insert_sorted(listNode *first, int x);
+
+void clear(listNode *first);
+
+void test(listNode *first);
